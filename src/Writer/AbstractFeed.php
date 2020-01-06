e6fa936004abdc8a99c297702ff00c4ca086a547
Masterflash <sxq@yandex.ru>
2020-01-06 17:35:49 +0300

    миграция на Laminas

--- src/Writer/AbstractFeed.php
+++ src/Writer/AbstractFeed.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Mf\FeedYaTurbo\Writer;
@@ -12,7 +7,7 @@ namespace Mf\FeedYaTurbo\Writer;
 use DateTime;
 use DateTimeInterface;
 use Mf\FeedYaTurbo\Uri;
-use Zend\Validator;
+use \Validator;
 
 class AbstractFeed
 {
